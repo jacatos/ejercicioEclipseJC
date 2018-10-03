62d2297377db42d38797c48eaf403aa81af36c0b
jacatos <prueba@prueba.com>
2018-10-03 18:47:49 +0200

    Mergeo y cambio

--- src/ejercicioEclipseJC/HolaMundoGit.java
+++ src/ejercicioEclipseJC/HolaMundoGit.java
@@ -4,7 +4,7 @@ public class HolaMundoGit {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hola Mundo Git desde github!!!");
+		System.out.println("Hola Mundo Git desde local y github!!!");
 	}
 
 }
